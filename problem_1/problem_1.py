fb512f9059415c380fd238c7eb549b01c0af64a2
Raninho <Raninho@Raninho.com.br>
2012-10-31 01:25:58 -0200

    Teste validado com sucesso

---
+++ problem_1/problem_1.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+class TestSumMultiples(unittest.TestCase):
+    ''' Problem 1 - 05 October 2001
+    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    we get 3, 5, 6 and 9. The sum of these multiples is 23.'''
+    def setUp(self):
+        self.result = (sum_multiples(10, 3) + sum_multiples(10, 5))
+
+    def test_sum(self):
+        print(self.result)
+        self.assertEqual(self.result, 23)
+
+
+def sum_multiples(limit, multiple):
+    sum = 0
+    for number in xrange(1, limit):
+        print(number)
+        if (number % multiple) == 0:
+            sum += number
+    return sum
+
+if __name__ == '__main__':
+    ''' Find the sum of all the multiples of 3 or 5 below 1000.'''
+    print(sum_multiples(1000, 3) + sum_multiples(1000, 5))
