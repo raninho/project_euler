a4b5354681e4af7b8c29a47b62580ef3c2fa918d
Raninho <Raninho@Raninho.com.br>
2012-10-31 23:25:19 -0200

    Correção no algoritmo para não repitir a soma do mesmo número multiplo de 3 ou 5

--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -6,21 +6,25 @@ class TestSumMultiples(unittest.TestCase):
     If we list all the natural numbers below 10 that are multiples of 3 or 5,
     we get 3, 5, 6 and 9. The sum of these multiples is 23.'''
     def setUp(self):
-        self.result = (sum_multiples(10, 3) + sum_multiples(10, 5))
+        self.result = sum_multiples(10)
 
     def test_sum(self):
-        print(self.result)
         self.assertEqual(self.result, 23)
 
 
-def sum_multiples(limit, multiple):
+def sum_multiples(limit):
     sum = 0
     for number in xrange(1, limit):
-        print(number)
-        if (number % multiple) == 0:
+        multiple_3 = (number % 3)
+        multiple_5 = (number % 5)
+        if multiple_3 == 0 and multiple_5 == 0:
+            sum += number
+        elif multiple_3 == 0:
+            sum += number
+        elif multiple_5 == 0:
             sum += number
     return sum
 
 if __name__ == '__main__':
     ''' Find the sum of all the multiples of 3 or 5 below 1000.'''
-    print(sum_multiples(1000, 3) + sum_multiples(1000, 5))
+    print(sum_multiples(1000))
